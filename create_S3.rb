afcb01de67d34205724af199302713e4ba0390d0
Tomoko Sohda <tomoko.s.0000@gmail.com>
2012-07-20 05:55:03 +0000

    rename local ariable

--- create_S3.rb
+++ create_S3.rb
@@ -3,5 +3,5 @@ require 'aws-sdk'
 s3 = AWS::S3.new(:access_key_id     => 'XXACCESS_KEY_IDXX',
                  :secret_access_key => 'XXSECRET_ACCESS_KEYXX')
 
-uploader000 = s3.buckets.create('your_bucket')
-uploader000.objects['this/is/not/a/directry/tmp.txt'].write "this is not a directry"
+bucket = s3.buckets.create('your_bucket')
+bucket.objects['this/is/not/a/directry/tmp.txt'].write "this is not a directry"
